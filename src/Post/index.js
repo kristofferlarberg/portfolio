50413d1ca3850ec88d5ce76e9f6144ca90cb31d0
Kristoffer Larberg <mail@kristofferlarberg.se>
2020-02-09 22:32:39 +0100

    Fixed margins in general and typesizes on mobile.

--- src/Post/index.js
+++ src/Post/index.js
@@ -12,19 +12,21 @@ const Post = ({ projects, showDetails }) => {
 
       <section className="description">
         <h1>{item.title}</h1>
-        <figure><img src={item.img} alt='Documentation'></img></figure>
       </section>
+        <figure><img src={item.img} alt='Documentation'></img></figure>
       <section className='description'>
         <p>{item.description}</p>
         <p>{item.url ? <a href={item.url}>{item.url}</a> : null}</p>
-        <footer>
-          <h3>Role: {item.role}</h3>
-          <h3>{item.team ? `Team: ${item.team}` : null}</h3>
-          <h3>Format: {item.format}</h3><h3>Method: {item.method}</h3>
-          <h3>{item.github ? <a href={item.github} className='github'><img src='img/github.svg' span style={{ width: '50px' }}></img></a> : null}</h3>
-        </footer>
+        
       </section>
-      
+      <footer>
+        <h3>Role: {item.role}</h3>
+        <h3>{item.team ? `Team: ${item.team}` : null}</h3>
+        <h3>Format: {item.format}</h3><h3>Method: {item.method}</h3>
+        <h3>{item.github ? <a href={item.github} className='github'><img src='img/github.svg' span style={{ width: '50px' }}></img></a> : null}</h3>
+      </footer>
+      <div style={{ width: '100%', borderBottom: '1px solid black'}}></div>
+
     </article >
   )
 }
